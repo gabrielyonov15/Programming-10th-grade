a3b62b96d2cf597b6525e4b07cca14e7e3b5e618
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-29 12:57:53 +0200

    Add files via upload

---
+++ PrintNumbersWithinARange/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrintNumbersWithinARange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; 
+            int b = 20; 
+            for (int i = a + 1; i < b; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ PrintPositiveEvenNumbersLessThanN/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PrintPositiveEvenNumbersLessThanN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 20; 
+            for (int i = 2; i < N; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
